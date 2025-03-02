12fa597170d26d080df3618b405217c2bd4fcce5
rvngzd00 <rvngzd@gmail.com>
2025-03-03 00:25:57 +0400

    updated Front

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,6 @@
 let postingPrompt = ""; // Ai a Gedecek Olan Promt
 let selectedColor = ""; 
 const generatePrompt = document.getElementById('generatePrompt');
-const generatedPrompt = document.getElementById('generatedPrompt');
 function generatePromt() {
     if(selectedColor){
         postingPrompt = `i want to make a combine for tomorrow, based on this color ${selectedColor}.`;
